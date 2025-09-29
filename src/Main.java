2e4cc1f5de4497f6bdc053bb7c71ff6bd639464c
markup54 <marzenakupka@tlen.pl>
2025-09-29 13:41:19 +0200

    losowanie bez powtorzen

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Main {
@@ -12,12 +14,60 @@ public class Main {
 
         //losuj 30 liczb z zakresu 1,40 bez powtorzen
 
+        ArrayList<Integer> wylosowaneLiczby = wylosujLiczbyBezPowtorzenDoListy(30);
+        wypisz(wylosowaneLiczby);
+
+        HashSet<Integer> wylosowaneLiczbyZbior = wylosujLiczbyBezPowtorzenDoZbioru(20);
+        wypisz(wylosowaneLiczbyZbior);
+    }
+
+    private static HashSet<Integer>wylosujLiczbyBezPowtorzenDoZbioru(int ileLiczb){
+        HashSet<Integer> liczbyWylosowane = new HashSet<>();
+        //elementy w zbiorze się nie powtarzają
+        //elementy są nie indeksowane, nieuporządkowane
+        Random random = new Random();
+        while (liczbyWylosowane.size()<ileLiczb){
+            int liczba = random.nextInt(20);
+            liczbyWylosowane.add(liczba);
+        }
+        return liczbyWylosowane;
+    }
+
+    private static ArrayList<Integer> wylosujLiczbyBezPowtorzenDoListy(int ileLiczb){
+        ArrayList<Integer> liczbyWylosowane = new ArrayList<>();
+        //nie musi być znany rozmiar listy,
+        //może być zmieniony w trakcie działania programu
+        //w każdej kolekcji tylko typy zlozone
+        //typ zlozony pisany wielką literą
+        Random random = new Random();
+        for (int i = 0; i < ileLiczb; i++) {
+            int liczba =random.nextInt(40)+1;
+            while (liczbyWylosowane.contains(liczba)){
+                liczba = random.nextInt(40)+1;
+            }
+            liczbyWylosowane.add(liczba);
+        }
+        return liczbyWylosowane;
     }
     private static void wypisz(int[] tablica){
         System.out.println("Wylosowana tablica:");
         for (int i = 0; i < tablica.length; i++) {
             System.out.print(tablica[i]+", ");
         }
+    }
+private static void wypisz(HashSet<Integer> zbior){
+    System.out.println("Wypisane liczby ze zbioru");
+    for (int liczba:zbior) {
+        System.out.print(liczba+", ");
+    }
+    System.out.println();
+}
+    private static void wypisz(ArrayList<Integer> lista){
+        System.out.println("Wypisywana lista:");
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.print(lista.get(i)+", ");
+        }
+        System.out.println();
     }
 
     private static int[] losujLiczbyDoTablicy(int ileLiczb){
